808700e1dcb86581087cce72f418971c5076576e
Lorenzo Joaquin <lorenzojoaquin@MacBook-Pro-de-lorenzo.local>
2019-11-03 22:41:47 -0600

    new module

--- greekinator/urls.py
+++ greekinator/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from sentimentAnalysis import views
 from sentimentAnalysis import textForm
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
